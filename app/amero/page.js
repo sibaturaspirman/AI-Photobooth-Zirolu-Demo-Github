af694b4361129cdfb10cd76253e3ae32da0a80f4
Asep Irman <asepirman@antigravity.id>
2024-03-06 18:38:50 +0700

    fix gambar

--- app/amero/page.js
+++ app/amero/page.js
@@ -11,13 +11,13 @@ export default function AmeroHome() {
       <h1 className={`text-center text-xl font-bold mt-[-.7rem] lg:mt-0 lg:text-7xl lg:mb-5 ${merriweather.className}`}>AI PHOTOBOOTH</h1>
       <div className="relative w-full flex justify-center items-center mt-5 mb-6 lg:mt-20 lg:mb-14">
         <div className='animate-upDown relative w-1/3 mx-auto flex justify-center items-center pointer-events-none'>
-          <Image src='/avatar/preview-1.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
+          <Image src='/amero/preview-1.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
         </div>
         <div className='animate-upDown2 relative w-1/3 mx-auto flex justify-center items-center pointer-events-none'>
-          <Image src='/avatar/preview-2.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
+          <Image src='/amero/preview-2.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
         </div>
         <div className='animate-upDown3 relative w-1/3 mx-auto flex justify-center items-center pointer-events-none'>
-          <Image src='/avatar/preview-3.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
+          <Image src='/amero/preview-3.png' width={268} height={646} alt='Zirolu' className='w-full' priority />
         </div>
       </div>
       <div className="relative w-full flex justify-center items-center lg:mt-20">
