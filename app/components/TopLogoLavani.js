36514dbc29d59b4cf3620abc95e09771453de43e
Asep Irman <asepirman@antigravity.id>
2024-03-07 07:44:39 +0700

    loogo home

--- app/components/TopLogoLavani.js
+++ app/components/TopLogoLavani.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const TopLogoLavani = () => {
   return (
-    <a href='/' className='relative w-[120px] lg:w-[50%] mx-auto flex justify-center items-center z-50'>
+    <a href='/amero' className='relative w-[120px] lg:w-[50%] mx-auto flex justify-center items-center z-50'>
       <Image src='/amero/logo-l.png' width={314} height={134} alt='Zirolu' className='w-full' priority />
     </a>
   );
--- app/components/TopLogoMorraine.js
+++ app/components/TopLogoMorraine.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const TopLogoMorraine = () => {
   return (
-    <a href='/' className='relative w-[120px] lg:w-[50%] mx-auto flex justify-center items-center z-50'>
+    <a href='/amero' className='relative w-[120px] lg:w-[50%] mx-auto flex justify-center items-center z-50'>
       <Image src='/amero/logo-m.png' width={314} height={134} alt='Zirolu' className='w-full' priority />
     </a>
   );
