37dca30f4cfba3842ee3e79b493b14424f2f4527
Asep Irman <asepirman@antigravity.id>
2024-04-26 12:51:49 +0700

    cha nge

--- app/hms-farewell/style/page.js
+++ app/hms-farewell/style/page.js
@@ -34,12 +34,12 @@ export default function Register() {
         let urlGambar3 = '';
         if(character == 'formasi-1'){
             let randomGambar = getRandomInt(1, 3);
-            // urlGambar = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-left.jpeg';
-            // urlGambar2 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-center.jpeg';
-            // urlGambar3 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-right.jpeg';
-            urlGambar = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-left.jpeg';
-            urlGambar2 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-center.jpeg';
-            urlGambar3 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-right.jpeg';
+            urlGambar = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-left.jpeg';
+            urlGambar2 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-center.jpeg';
+            urlGambar3 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+randomGambar+'-right.jpeg';
+            // urlGambar = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-left.jpeg';
+            // urlGambar2 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-center.jpeg';
+            // urlGambar3 = 'https://ai.zirolu.id/hms/style/fcfix-1-'+1+'-right.jpeg';
             console.log(randomGambar)
 
             if (typeof localStorage !== 'undefined') {
