a11e0ab61773163b4458e9dd8fc39cf660c32e3f
Asep Irman <asepirman@antigravity.id>
2024-06-19 07:13:10 +0700

    add vton

---
+++ app/vton/page.js
@@ -0,0 +1,216 @@
+'use client';
+
+import * as fal from '@fal-ai/serverless-client';
+import Image from "next/image";
+import { setCookie } from 'cookies-next';
+import { useState, useMemo } from 'react';
+import TopLogo from "../components/TopLogo";
+import BtnHexagon from "../components/BtnHexagon";
+import { useRouter } from 'next/navigation';
+
+// @snippet:start(client.config)
+fal.config({
+    // credentials: 'FAL_KEY_ID:FAL_KEY_SECRET',
+    requestMiddleware: fal.withProxy({
+      targetUrl: '/api/fal/proxy', // the built-int nextjs proxy
+      // targetUrl: 'http://localhost:3333/api/fal/proxy', // or your own external proxy
+    }),
+});
+
+let FACE_URL_RESULT = ''
+
+export default function FaceSwap() {
+    const router = useRouter();
+
+    // Result state
+    const [numProses, setNumProses] = useState(0);
+    const [numProses1, setNumProses1] = useState();
+    const [baseFaceFile, setBaseFaceFile] = useState(null);
+    const [swapFaceFile, setSwapFaceFile] = useState(null);
+    const [desc, setDesc] = useState('Short Sleeve Round Neck');
+    const [seed, setSeed] = useState(42);
+    const [step, setStep] = useState(30);
+    const [linkURL, setLinkURL] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [result, setResult] = useState(null);
+    const [resultFaceSwap, setResultFaceSwap] = useState(null);
+    const [logs, setLogs] = useState([]);
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    const generateAI = () => {
+        generateImageSwap()
+    }
+
+
+    const imageFaceSwap = useMemo(() => {
+        if (!resultFaceSwap) {
+        return null;
+        }
+        if (resultFaceSwap.image) {
+        return resultFaceSwap.image;
+        }
+        return null;
+    }, [resultFaceSwap]);
+
+    const reset2 = () => {
+        setNumProses(2)
+        setResultFaceSwap(null);
+        setLoading(false);
+        setError(null);
+        // setLogs([]);
+        setElapsedTime(0);
+      };
+    const generateImageSwap = async () => {
+        setNumProses(2)
+        reset2();
+        // @snippet:start("client.queue.subscribe")
+        setLoading(true);
+        const start = Date.now();
+        try {
+            const result = await fal.subscribe(
+                'fal-ai/idm-vton',
+                {
+                input: {
+                    human_image_url: baseFaceFile,
+                    garment_image_url: swapFaceFile,
+                    description: desc,
+                    seed: seed,
+                    num_inference_stepsinteger: step,
+                },
+                pollInterval: 5000, // Default is 1000 (every 1s)
+                logs: true,
+                onQueueUpdate(update) {
+                    setElapsedTime(Date.now() - start);
+                    if (
+                    update.status === 'IN_PROGRESS' ||
+                    update.status === 'COMPLETED'
+                    ) {
+                    setLogs((update.logs || []).map((log) => log.message));
+                    }
+                },
+                }
+            );
+            setResultFaceSwap(result);
+            FACE_URL_RESULT = result.image.url;
+            setLinkURL(result.image.url)
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+            setElapsedTime(Date.now() - start);
+        }
+        // @snippet:end
+    };
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-top pt-12 px-4 lg:p-20">
+            <TopLogo></TopLogo>
+            <div className="relative w-full flex flex-col justify-center items-center mt-4 mb-0">
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="base" className="text-[#D8BA78] font-bold text-2xl mb-2 block">Base Photo (768x1024px)</label>
+                    <div className='relative w-full'>
+                        <input
+                            type='file'
+                            id='base'
+                            name='base'
+                            className={`w-full border-2 border-[#D8BA78] rounded-sm font-semibold text-2xl outline-none py-2 pr-3 text-white bg-black bg-opacity-[4%]'`}
+                            placeholder='Base Photo'
+                            accept="image/*;capture=camera"
+                            onChange={(e) => setBaseFaceFile(e.target.files?.[0] ?? null)}
+                        />
+                    </div>
+                </div>
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="swap" className="text-[#D8BA78] font-bold text-2xl mb-2 block">Garment/Baju (768x1024px)</label>
+                    <div className='relative w-full'>
+                        <input
+                            type='file'
+                            id='swap'
+                            name='swap'
+                            className={`w-full border-2 border-[#D8BA78] rounded-sm font-semibold text-2xl outline-none py-2 pr-3 text-white bg-black bg-opacity-[4%]'`}
+                            placeholder='Garment/Baju'
+                            accept="image/*;capture=camera"
+                            onChange={(e) => setSwapFaceFile(e.target.files?.[0] ?? null)}
+                        />
+                    </div>
+                    {/* {payload.phone} */}
+                    {/* {errorMsg && <p className='text-[#E00A0A] text-xs'>{errorMsg}</p>} */}
+                </div>
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="desc" className="text-[#D8BA78] font-bold text-2xl mb-2 block">Description Garment/Baju</label>
+                    <div className='relative w-full'>
+                        <input
+                            type='text'
+                            id='desc'
+                            name='desc'
+                            className={`w-full border-2 border-[#D8BA78] rounded-sm font-semibold text-2xl outline-none py-2 px-3 text-white bg-black bg-opacity-[4%]'`}
+                            placeholder='description'
+                            value={desc}
+                            onChange={(e) => setDesc(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="seed" className="text-[#D8BA78] font-bold text-2xl mb-2 block">Seed</label>
+                    <div className='relative w-full'>
+                        <input
+                            type='number'
+                            id='seed'
+                            name='seed'
+                            className={`w-full border-2 border-[#D8BA78] rounded-sm font-semibold text-2xl outline-none py-2 px-3 text-white bg-black bg-opacity-[4%]'`}
+                            placeholder='seed'
+                            value={seed}
+                            onChange={(e) => setSeed(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="step" className="text-[#D8BA78] font-bold text-2xl mb-2 block">Steps</label>
+                    <div className='relative w-full'>
+                        <input
+                            type='number'
+                            id='step'
+                            name='step'
+                            className={`w-full border-2 border-[#D8BA78] rounded-sm font-semibold text-2xl outline-none py-2 px-3 text-white bg-black bg-opacity-[4%]'`}
+                            placeholder='step'
+                            value={step}
+                            onChange={(e) => setStep(e.target.value)}
+                        />
+                    </div>
+                </div>
+                {/* {step}/{seed} */}
+                {imageFaceSwap && 
+                <div className='relative w-full lg:w-[80%] mb-6'>
+                    <label htmlFor="swap" className="text-[#D8BA78] font-bold text-2xl mb-4 block">Result</label>
+                    <a href={linkURL} target='_blank' className="relative w-[250px] mb-2 flex justify-center items-center">
+                            <Image src='/btn-download-image.png' width={410} height={96} alt='Zirolu' className='w-full' priority />
+                    </a>
+                    <div><Image src={imageFaceSwap.url} width={768} height={1024} className='w-full' alt='zirolu'></Image></div>
+                </div>
+                }
+            </div>
+            <div className="relative w-[80%] flex justify-center items-center">
+                <button className="relative mx-auto w-[70%] flex justify-center items-center" onClick={generateAI}>
+                    <Image src='/btn-generate.png' width={410} height={96} alt='Zirolu' className='w-full' priority />
+                </button>
+            </div>
+            {numProses == 2 && 
+                <div className='relative w-[80%]'>
+                    <div className='animate-upDownCepet relative py-2 px-4 mt-5 border-2 border-[#ffffff] text-center bg-slate-500 text-[#fff] lg:font-bold w-full'>
+                        <p>{`Please wait, loading...`}</p>
+                        <p>{`Process : ${(elapsedTime / 1000).toFixed(2)} seconds (${numProses} of 2)`}</p>
+                        {error}
+                    </div>
+
+                    <pre className='w-full relative py-2 px-4 mt-5 lg:mt-24 border-2 border-[#ffffff] text-left bg-slate-500 text-[#fff] text-xs lg:text-sm overflow-auto no-scrollbar h-[100px] mx-auto'>
+                        <code>
+                        {logs.filter(Boolean).join('\n')}
+                        </code>
+                        AI generate garment... <br></br>
+                        Loading model..<br></br>
+                    </pre>
+                </div>
+            }
+        </main>
+    );
+}
