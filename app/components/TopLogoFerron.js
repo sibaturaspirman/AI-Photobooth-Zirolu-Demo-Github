aeffbba2e07174c2b6033f1bc9ad64f32792f63f
Asep Irman <asepirman@antigravity.id>
2024-07-05 16:48:55 +0700

    logo

--- app/components/TopLogoFerron.js
+++ app/components/TopLogoFerron.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const TopLogoFerron = () => {
   return (
-    <a href='/ferron' className='relative w-[20%] mx-auto flex justify-center items-center z-50'>
-      <Image src='/dexa/ferron.png' width={629} height={501} alt='Zirolu' className='w-full' priority />
+    <a href='/ferron' className='relative w-[12%] mx-auto flex justify-center items-center z-50'>
+      <Image src='/dexa/ferron2.png' width={312} height={333} alt='Zirolu' className='w-full' priority />
     </a>
   );
 };
