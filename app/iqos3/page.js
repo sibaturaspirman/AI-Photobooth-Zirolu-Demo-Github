e13f5e52c48ed0d30be79fa8089f0ada86941619
Asep Irman <asepirman@antigravity.id>
2024-07-18 18:07:25 +0700

    upd

--- app/iqos3/page.js
+++ app/iqos3/page.js
@@ -25,9 +25,9 @@ export default function IQOSHome() {
   // useWebcam({ videoRef,previewRef});
 
   return (
-    <Link href='/iqos3/style' className="flex fixed h-full w-full bg-iqos-neon overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-0 lg:px-20 mt-0" onContextMenu={(e)=> e.preventDefault()}>
+    <Link href='/iqos3/style' className="flex fixed h-full w-full bg-iqos-neon overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-0 lg:px-20 mt-0">
       {/* <div className="fixed top-0 left-0 w-full h-full bg-iqos-border pointer-events-none z-10"></div> */}
-      <div className="relative w-[75%] mx-auto mt-0">
+      <div className="relative w-[75%] mx-auto mt-0 pointer-events-none">
         <Image src='/iqos/neon/title.png' width={803} height={256} alt='Zirolu' className='w-full' priority />
       </div>
       <div className="relative w-full flex justify-center items-center mt-5 mb-6 lg:mt-24 lg:mb-14 opacity-0">
@@ -35,7 +35,7 @@ export default function IQOSHome() {
           <Image src='/iqos/preview2.png' width={864} height={721} alt='Zirolu' className='w-full' priority />
         </div>
       </div>
-      <div className="relative w-full flex justify-center items-center lg:mt-20">
+      <div className="relative w-full flex justify-center items-center lg:mt-20 pointer-events-none">
         <div className="relative mx-auto flex w-[68%] justify-center items-center">
           <Image src='/iqos/neon/tap.png' width={731} height={39} alt='Zirolu' className='w-full' priority />
         </div>
