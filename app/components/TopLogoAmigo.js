d046ed071ba79a2dc0204ae998285eed159b8d9e
Asep Irman <asepirman@antigravity.id>
2024-08-22 19:52:01 +0700

    takeoutlogo

--- app/components/TopLogoAmigo.js
+++ app/components/TopLogoAmigo.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 const TopLogoAmigo = () => {
   return (
-    <a href='/pertamina' className='relative w-[120px] lg:w-[120px] mx-auto flex justify-center items-center z-50'>
-      <Image src='/porseni/amigo.png' width={234} height={100} alt='Zirolu' className='w-full' priority />
-    </a>
+    // <a href='/pertamina' className='relative w-[120px] lg:w-[120px] mx-auto flex justify-center items-center z-50'>
+    //   <Image src='/porseni/amigo.png' width={234} height={100} alt='Zirolu' className='w-full' priority />
+    // </a>
+    <div></div>
   );
 };
 
