c9f031cb81443a68a26a723feaf7c101f950b197
Asep Irman <asepirman@antigravity.id>
2024-11-06 07:07:58 +0800

    upd

--- app/components/TopLogoMizuho.js
+++ app/components/TopLogoMizuho.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const TopLogoMizuho = () => {
   return (
-    <a href='/mizuho' className='relative w-[120px] lg:w-[190px] mx-auto flex justify-center items-center z-50'>
+    <a href='/mizuho' className='relative w-[120px] lg:w-[190px] mx-auto flex justify-center items-center z-50 opacity-0 hidden'>
       <Image src='/mizuho/logo.png' width={234} height={100} alt='Zirolu' className='w-full' priority />
     </a>
   );
