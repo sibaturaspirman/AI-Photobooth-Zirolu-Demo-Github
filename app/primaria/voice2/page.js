c5aff106ab097ed6f50ee0f8b9aabdda135e325a
Asep Irman <asepirman@antigravity.id>
2025-01-13 15:40:18 +0700

    upd copy

--- app/primaria/voice2/page.js
+++ app/primaria/voice2/page.js
@@ -17,7 +17,7 @@ export default function Voice() {
         <main className="flex fixed h-full w-full bg-primaria overflow-auto flex-col items-center pt-2 pb-5 px-5 lg:pt-12 lg:px-20">
             <BgWave ></BgWave>
             <TopLogoPrimaria></TopLogoPrimaria>
-            <h1 className={`text-center text-xl lg:text-6xl font-medium mt-1 lg:mt-10 z-20 !leading-snug ${kanit.className}`}>Ketawain masalah lo sekenceng-kencengnya sampai semangatmu terisi penuh!</h1>
+            <h1 className={`text-center text-xl lg:text-6xl font-medium mt-1 lg:mt-10 z-20 !leading-snug ${kanit.className}`}>Ayo ketawa sampe semangat lo terisi penuh</h1>
             <div className="relative w-full flex flex-col justify-center items-center z-20">
                 <VoiceWave direct={'cam2'} />
             </div>
