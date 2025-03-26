79f9467bdaa177bf4525474cb114bf4054683e6c
Asep Irman <asepirman@antigravity.id>
2025-03-26 17:20:12 +0700

    tablet

---
+++ app/dss-rehat/tablet/apakah/page.js
@@ -0,0 +1,138 @@
+'use client';
+
+import Link from 'next/link';
+// import * as fal from '@fal-ai/serverless-client';
+import Image from "next/image";
+import { useEffect, useState, useMemo, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+// Import Swiper React components
+
+// Import Swiper styles
+import 'swiper/css';
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default function Apakah() {
+    const router = useRouter();
+    const [numProses1, setNumProses1] = useState(null);
+    const [Name, setName] = useState(null);
+    const [slideIndex, setSlideIndex] = useState(0);
+    const [slideIndex2, setSlideIndex2] = useState(0);
+    const [moodType, setMoodType] = useState('gakbutuhrehat');
+    const [swipeTrigger, setSwipeTrigger] = useState(false);
+    const [sliderProgress, setSliderProgress] = useState(0);
+    const [sliderProgress2, setSliderProgress2] = useState(0);
+    const [maxDuration, setMaxDuration] = useState(5);
+    const [countdownStart, setCountdownStart] = useState(false);
+    const [statusAPI, setStatusAPI] = useState();
+    const [urlVideo, setUrlVideo] = useState();
+    const timerRef = useRef(null);
+
+    useEffect(() => {
+        handleStartCountdown()
+        if(countdownStart){
+            if(maxDuration == 0){
+                setSwipeTrigger(true)
+            }
+        }
+    }, [swipeTrigger, countdownStart, maxDuration])
+
+    const generateAura = async () => {
+        // console.log(moodType)
+        if (typeof localStorage !== 'undefined') {
+            localStorage.setItem("moodFix", moodType)
+        }
+        setTimeout(() => {
+            router.push('/dss-rehat/tablet/genre');
+        }, 100);
+    }
+
+    const SetupSlider = (e) => {
+        // console.log(e)
+        const tempSliderValue = e.target.value; 
+        setSliderProgress(e.target.value)
+        const progress = (tempSliderValue / 100) * 100;
+        // console.log(progress)
+
+        let progressBagi = progress / 33;
+        setSlideIndex(progressBagi)
+        if(progressBagi == 0) setMoodType('gakbutuhrehat')
+        else if(progressBagi == 1) setMoodType('belumtahu')
+        else if(progressBagi == 2) setMoodType('mulaibutuh')
+        else if(progressBagi == 3) setMoodType('banget')
+
+        // console.log(moodType)
+    }
+
+    // COUNTDOWN
+    const handleStartCountdown = () => {
+        if (maxDuration <= 5) {
+            setCountdownStart(true);
+            timerRef.current = setInterval(() => {
+                setMaxDuration((prevTime) => {
+                  if (prevTime <= 1) {
+                    clearInterval(timerRef.current);
+                    return 0;
+                  }
+                  return prevTime - 1;
+                });
+            }, 1000);
+        }
+    };
+
+    return (
+        <main className="flex fixed h-full w-full bg-dss overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-12 lg:px-20" onContextMenu={(e)=> e.preventDefault()}>
+            <div className="relative w-[70%] mx-auto mt-0">
+            <Image src='/dss/apakah.png' width={720} height={260} alt='Zirolu' className='w-full' priority />
+            </div>
+            {/* PILIH STYLE */}
+            <div className={`relative w-[95%] mx-auto mt-10 z-20`}>
+                <div className="relative w-[800px] mx-auto z-20">
+                    <input type="range" 
+                    name="setupslider1"
+                    step="33"
+                    min="0"
+                    max="100"
+                    value={sliderProgress} 
+                    onInput={(e) =>  SetupSlider(e) }
+                    className="sliderProgressDss"/>
+                    <Image src='/dss/slider-progress.png' width={824} height={37} alt='Zirolu' className='w-full pointer-events-none' priority />
+                </div>
+                <div className='relative mt-0 w-[780px] mx-auto z-10'>
+                    <div className={`relative top-0 left-0 w-full pointer-events-none z-10 transition-all ${slideIndex == 0 ? `` : 'opacity-0'}`}>
+                        <Image src='/dss/slider-1.png' width={800} height={130} alt='Zirolu' className='w-full' priority />
+                    </div>
+                    <div className={`absolute top-0 left-0 w-full pointer-events-none z-10 transition-all ${slideIndex == 1 ? `` : 'opacity-0'}`}>
+                        <Image src='/dss/slider-2.png' width={800} height={130} alt='Zirolu' className='w-full' priority />
+                    </div>
+                    <div className={`absolute top-0 left-0 w-full pointer-events-none z-10 transition-all ${slideIndex == 2 ? `` : 'opacity-0'}`}>
+                        <Image src='/dss/slider-3.png' width={800} height={130} alt='Zirolu' className='w-full' priority />
+                    </div>
+                    <div className={`absolute top-0 left-0 w-full pointer-events-none z-10 transition-all ${slideIndex == 3 ? `` : 'opacity-0'}`}>
+                        <Image src='/dss/slider-4.png' width={800} height={130} alt='Zirolu' className='w-full' priority />
+                    </div>
+                </div>
+
+                {/* {character && */}
+                    <div className={`relative w-full flex justify-center items-center mt-[9rem] z-20`}>
+                        <button className="relative mx-auto w-[100%] flex justify-center items-center" onClick={generateAura}>
+                            <Image src='/dss/btn-continue.png' width={850} height={258} alt='Zirolu' className='w-full' priority />
+                        </button>
+                    </div>
+                {/* } */}
+
+            </div>
+            {/* !PILIH STYLE */}
+            <div className={`fixed top-0 bottom-0 left-0 right-0 w-full z-40 flex justify-center items-end pointer-events-none bg-black/0 transition-all ${swipeTrigger ? `opacity-0 pointer-events-none` : ''}`} onClick={()=> setSwipeTrigger(true)}>
+                <div className="relative p-20 pb-[2rem] w-full z-40 swipeInfo">
+                    <Image src='/comcon/veev/hand.png' width={202} height={200} alt='Zirolu' className='w-[20%] mx-auto mb-10 swipeInfoHand' priority />
+                    <Image src='/comcon/veev/swipe.png' width={702} height={124} alt='Zirolu' className='w-[60%] mx-auto' priority />
+                </div>
+            </div>
+        </main>
+    );
+}
---
+++ app/dss-rehat/tablet/page.js
@@ -0,0 +1,95 @@
+'use client';
+import Image from "next/image";
+import Link from 'next/link';
+import { useState } from 'react';
+// import BgWave from "../components/BgWave";
+import { useRouter } from 'next/navigation';
+
+export default function PrimariaHome() {
+  const router = useRouter();
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+  const [isValid, setIsValid] = useState(null);
+
+  const months = [
+    'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+    'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+  ];
+
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+
+  const handleValidation = () => {
+    if (!month || !year) {
+      setIsValid(null);
+      return;
+    }
+    const age = currentYear - year - (currentMonth < month ? 1 : 0);
+    if (age > 21) {
+      // router.push('/dss-rehat/home'); // Ganti dengan halaman tujuan
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
+  return (
+    <div className="flex fixed bg-dss-frontMOD h-full w-full overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-0 lg:px-20 mt-0">
+      {/* <div className="relative w-full flex justify-center items-center flex-col top-[-6rem]">
+        <div className='animate-upDown relative w-[90%] mx-auto flex justify-center items-center pointer-events-none'>
+          <Image src='/primaria/logo.png' width={880} height={347} alt='Zirolu' className='w-full' priority />
+        </div>
+        <div className="animate-upDown2 relative w-[75%] mx-auto mt-10">
+          <Image src='/primaria/tagline.png' width={926} height={41} alt='Zirolu' className='w-full' priority />
+        </div>
+      </div>
+      <div className="absolute left-0 right-0 bottom-[8rem] w-full flex justify-center items-center">
+        <div className="animate-upDownCepet relative mx-auto flex w-[78%] justify-center items-center">
+          <Image src='/primaria/btn-mulai.png' width={819} height={126} alt='Zirolu' className='w-full' priority />
+        </div>
+      </div> */}
+      <Link href='/dss-rehat/tablet/gender' className={`flex fixed h-full w-full overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-0 lg:px-20 mt-0 ${isValid ? '' : 'opacity-0 pointer-events-none'}`}></Link>
+
+      <div className={`flex fixed h-full w-full overflow-auto flex-col items-center justify-center pt-2 pb-5 px-5 lg:pt-0 lg:px-20 mt-0 bg-black/90 ${isValid ? 'opacity-0 pointer-events-none' : ''}`}>
+        <div className="relative flex justify-center items-center flex-col bg-black border-4 border-[#ECC953] w-[80%] mx-auto py-10 px-10">
+          <div className='relative w-[90%] mx-auto flex justify-center items-center pointer-events-none'>
+            <Image src='/dss/seldat.png' width={638} height={296} alt='Zirolu' className='w-full' priority />
+          </div>
+          <div className='relative w-full mx-auto flex justify-center items-center mt-5'>
+            <select 
+              className="border-[#ECC953] border-4 p-4 w-[80%] bg-white text-black text-3xl bg-transparent" 
+              value={month} 
+              onChange={(e) => setMonth(Number(e.target.value))}
+            >
+              <option value="">Pilih Bulan</option>
+              {months.map((monthName, i) => (
+                <option key={i + 1} value={i + 1}>{monthName}</option>
+              ))}
+            </select>
+            <select 
+              className="border-[#ECC953] border-4 p-4 w-[80%] bg-white text-black ml-4 text-3xl bg-transparent" 
+              value={year} 
+              onChange={(e) => setYear(Number(e.target.value))}
+            >
+              <option value="">Pilih Tahun</option>
+              {[...Array(76)].map((_, i) => (
+                <option key={2025 - i} value={2025 - i}>{2025 - i}</option>
+              ))}
+            </select>
+          </div>
+          <button 
+            className="mt-6 bg-transparent" 
+            onClick={handleValidation}
+          >
+            <Image src='/dss/konfirmasi.png' width={638} height={88} alt='Zirolu' className='w-full' priority />
+          </button>
+          {isValid !== null && (
+            <p className={`mt-7 text-5xl font-semibold ${isValid ? 'text-green-500' : 'text-red-500'}`}>
+              {isValid ? '' : 'Usia kurang dari 21 tahun'}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
