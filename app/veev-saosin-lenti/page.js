9d41205a104b2258efcce0b052598172b2004d9a
Asep Irman <asepirman@antigravity.id>
2025-05-22 15:10:46 +0700

    add saosin

---
+++ app/veev-saosin-lenti/page.js
@@ -0,0 +1,24 @@
+
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function IQOSHome() {
+  return (
+    <div className="flex fixed h-full w-full bg-veev-lenti overflow-auto flex-col items-center justify-center">
+      <div className="fixed top-0 left-0 w-full h-full bg-iqos-border pointer-events-none z-10 hidden lg:block"></div>
+      {/* <div className="relative w-[63%] mt-[-12rem]">
+        <div className='relative w-full mx-auto flex justify-center items-center'>
+          <Image src='/greenday/v-logo.png' width={787} height={84} alt='Zirolu' className='w-full' priority />
+        </div>
+      </div> */}
+      {/* <div className="relative w-full">
+        <Image src='/comcon/veev/preview.png' width={787} height={84} alt='Zirolu' className='w-full' priority />
+      </div> */}
+      <div className="absolute mx-auto bottom-10 lg:bottom-[20rem] w-[80%] ">
+        <Link href='/veev-saosin-lenti/style' className='relative w-full mx-auto flex justify-center items-center'>
+          <Image src='/veev/lenti/btn-continue.png' width={880} height={144} alt='Zirolu' className='w-full' priority />
+        </Link>
+      </div>
+    </div>
+  );
+}
