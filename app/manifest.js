e111c82f2675359eabdb0170370ab8c6dd6ff432
Asep Irman <asepirman@antigravity.id>
2025-06-20 16:56:02 +0700

    init iqos prj

--- app/manifest.js
+++ app/manifest.js
@@ -3,7 +3,7 @@ export default function manifest() {
       name: 'AI PHOTOBOOTH',
       short_name: 'AI PHOTOBOOTH',
       description: 'AI PHOTOBOOTH',
-      start_url: '/zymuno',
+      start_url: '/iqos-prj',
       display: 'standalone',
       background_color: '#fff',
       theme_color: '#fff',
